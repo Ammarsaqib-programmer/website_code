7ee4e83c983664b74cfcffe3d7e272e032e38301
Ammar Arain <150575040+Ammarsaqib-programmer@users.noreply.github.com>
2025-12-23 21:39:28 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple form submission alert
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for contacting us! 🌿');
+    form.reset();
+});
